b439b6260b52c63f1092bd2b375906b3e36cecb9
TheOzzy <105451431+The0zzy@users.noreply.github.com>
2023-03-15 18:31:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
   const pluginId = "bf2042-portal-navigation-plugin";
   const plugin = BF2042Portal.Plugins.getPlugin(pluginId);
 
+  let pluginData = {scrollableWorkspace: false};
+
   const toggleScrollItem = {
     displayText: "Toggle Scroll Workspace",
     preconditionFn: () => "enabled",
     callback: function () {
       _Blockly.getMainWorkspace().options.moveOptions.wheel = !_Blockly.getMainWorkspace()
         .options.moveOptions.wheel;
+pluginData.scrollableWorkspace=_Blockly.getMainWorkspace().options.moveOptions.wheel;
+BF2042Portal.Shared.saveToLocalStorage(pluginId, pluginData);
     },
     scopeType: _Blockly.ContextMenuRegistry.ScopeType.WORKSPACE,
     id: "toggleScrollItem",
@@ -17,7 +21,11 @@
   plugin.initializeWorkspace = function () {
     logInfo("Initializing...");
     try {
-      _Blockly.ContextMenuRegistry.registry.register(toggleScrollItem);
+      let loadedData = BF2042Portal.Shared.loadFromLocalStorage(pluginId)
+      if(loadedData){
+pluginData = loadedData;
+}
+_Blockly.ContextMenuRegistry.registry.register(toggleScrollItem);
       logInfo("Initialized!");
     } catch (exception) {
       logError("Failed to initialize!", exception);
