c6e850529438c599d794efa8cd7191b4ba39dd84
Eric Wünsche <wuensche.e@gmail.com>
2023-03-16 21:15:29 +0100

    feat: store and load scroll behavior

--- index.js
+++ index.js
@@ -2,17 +2,29 @@
   const pluginId = "bf2042-portal-navigation-plugin";
   const plugin = BF2042Portal.Plugins.getPlugin(pluginId);
 
-  let pluginData = {scrollableWorkspace: false};
+  let pluginData = { scrollableWorkspace: false };
+
+  function toggleScrollableWorkspace() {
+    _Blockly.getMainWorkspace().options.moveOptions.wheel = !_Blockly.getMainWorkspace()
+      .options.moveOptions.wheel;
+    pluginData.scrollableWorkspace = _Blockly.getMainWorkspace().options.moveOptions.wheel;
+    logInfo(
+      "Scrollable Workspace set to " + pluginData.scrollableWorkspace
+        ? "TRUE"
+        : "FALSE"
+    );
+    try {
+      BF2042Portal.Shared.saveToLocalStorage(pluginId, pluginData);
+      logInfo("Saved current setting in local storage.");
+    } catch (error) {
+      logError("Couldn't save current setting in local storage.", error);
+    }
+  }
 
   const toggleScrollItem = {
     displayText: "Toggle Scroll Workspace",
     preconditionFn: () => "enabled",
-    callback: function () {
-      _Blockly.getMainWorkspace().options.moveOptions.wheel = !_Blockly.getMainWorkspace()
-        .options.moveOptions.wheel;
-pluginData.scrollableWorkspace=_Blockly.getMainWorkspace().options.moveOptions.wheel;
-BF2042Portal.Shared.saveToLocalStorage(pluginId, pluginData);
-    },
+    callback: toggleScrollableWorkspace,
     scopeType: _Blockly.ContextMenuRegistry.ScopeType.WORKSPACE,
     id: "toggleScrollItem",
     weight: 200,
@@ -21,11 +33,17 @@ BF2042Portal.Shared.saveToLocalStorage(pluginId, pluginData);
   plugin.initializeWorkspace = function () {
     logInfo("Initializing...");
     try {
-      let loadedData = BF2042Portal.Shared.loadFromLocalStorage(pluginId)
-      if(loadedData){
-pluginData = loadedData;
-}
-_Blockly.ContextMenuRegistry.registry.register(toggleScrollItem);
+      let loadedData = BF2042Portal.Shared.loadFromLocalStorage(pluginId);
+      if (loadedData) {
+        pluginData = loadedData;
+      }
+    } catch (exception) {
+      logError("Couldn't load settings from local storage!", exception);
+    }
+    try {
+      _Blockly.getMainWorkspace().options.moveOptions.wheel =
+        pluginData.scrollableWorkspace;
+      _Blockly.ContextMenuRegistry.registry.register(toggleScrollItem);
       logInfo("Initialized!");
     } catch (exception) {
       logError("Failed to initialize!", exception);
