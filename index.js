664df94f12481d6c57fa93cc7e7edd21625c781f
TheOzzy <105451431+The0zzy@users.noreply.github.com>
2023-03-16 21:16:15 +0100

    2 store default scroll behavior (#3)
    * Update index.js
    
    * feat: store and load scroll behavior

--- index.js
+++ index.js
@@ -2,13 +2,29 @@
   const pluginId = "bf2042-portal-navigation-plugin";
   const plugin = BF2042Portal.Plugins.getPlugin(pluginId);
 
+  let pluginData = { scrollableWorkspace: false };
+
+  function toggleScrollableWorkspace() {
+    _Blockly.getMainWorkspace().options.moveOptions.wheel = !_Blockly.getMainWorkspace()
+      .options.moveOptions.wheel;
+    pluginData.scrollableWorkspace = _Blockly.getMainWorkspace().options.moveOptions.wheel;
+    logInfo(
+      "Scrollable Workspace set to " + pluginData.scrollableWorkspace
+        ? "TRUE"
+        : "FALSE"
+    );
+    try {
+      BF2042Portal.Shared.saveToLocalStorage(pluginId, pluginData);
+      logInfo("Saved current setting in local storage.");
+    } catch (error) {
+      logError("Couldn't save current setting in local storage.", error);
+    }
+  }
+
   const toggleScrollItem = {
     displayText: "Toggle Scroll Workspace",
     preconditionFn: () => "enabled",
-    callback: function () {
-      _Blockly.getMainWorkspace().options.moveOptions.wheel = !_Blockly.getMainWorkspace()
-        .options.moveOptions.wheel;
-    },
+    callback: toggleScrollableWorkspace,
     scopeType: _Blockly.ContextMenuRegistry.ScopeType.WORKSPACE,
     id: "toggleScrollItem",
     weight: 200,
@@ -17,6 +33,16 @@
   plugin.initializeWorkspace = function () {
     logInfo("Initializing...");
     try {
+      let loadedData = BF2042Portal.Shared.loadFromLocalStorage(pluginId);
+      if (loadedData) {
+        pluginData = loadedData;
+      }
+    } catch (exception) {
+      logError("Couldn't load settings from local storage!", exception);
+    }
+    try {
+      _Blockly.getMainWorkspace().options.moveOptions.wheel =
+        pluginData.scrollableWorkspace;
       _Blockly.ContextMenuRegistry.registry.register(toggleScrollItem);
       logInfo("Initialized!");
     } catch (exception) {
