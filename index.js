4b895aa55924d546816874e2421d2bef68d3cd5d
Eric Wünsche <wuensche.e@gmail.com>
2023-06-25 22:06:58 +0200

    add "jump to" menu

--- index.js
+++ index.js
@@ -20,6 +20,123 @@
     }
   }
 
+  function buildJumpToMenu() {
+    let jumpToMenu = plugin.createMenu(
+      "jumpToMenu",
+      "Jump To",
+      _Blockly.ContextMenuRegistry.ScopeType.WORKSPACE
+    );
+    plugin.registerMenu(jumpToMenu);
+    jumpToMenu.options = [];
+
+    let modBlocks = _Blockly
+      .getMainWorkspace()
+      .getBlocksByType("modBlock", false);
+    let ruleBlocks = _Blockly
+      .getMainWorkspace()
+      .getBlocksByType("ruleBlock", true);
+    let subroutines = _Blockly
+      .getMainWorkspace()
+      .getBlocksByType("subroutineBlock", false)
+      .sort((a, b) =>
+        a
+          .getFieldValue("SUBROUTINE_NAME")
+          .localeCompare(b.getFieldValue("SUBROUTINE_NAME"))
+      );
+
+    let blockIndex = 1;
+    modBlocks.forEach((block) => {
+      let blockItem = {
+        displayText: blockIndex + " Mod",
+        preconditionFn: () => "enabled",
+        callback: () => {
+          jumpTo(block);
+        },
+        scopeType: _Blockly.ContextMenuRegistry.ScopeType.WORKSPACE,
+        id: block.id,
+        weight: blockIndex,
+      };
+      jumpToMenu.options.push("items." + blockItem.id);
+      plugin.registerItem(blockItem);
+      blockIndex += 1;
+    });
+    blockIndex = 1;
+
+    if (ruleBlocks.length > 0) {
+      let rulesMenu = plugin.createMenu(
+        "rulesMenu",
+        "Rules",
+        _Blockly.ContextMenuRegistry.ScopeType.WORKSPACE
+      );
+      plugin.registerMenu(rulesMenu);
+      rulesMenu.options = [];
+      ruleBlocks.forEach((block) => {
+        let blockItem = {
+          displayText: blockIndex + " " + block.getFieldValue("NAME"),
+          preconditionFn: () => "enabled",
+          callback: () => {
+            jumpTo(block);
+          },
+          scopeType: _Blockly.ContextMenuRegistry.ScopeType.WORKSPACE,
+          id: block.id,
+          weight: blockIndex,
+        };
+        rulesMenu.options.push("items." + blockItem.id);
+        plugin.registerItem(blockItem);
+        blockIndex += 1;
+      });
+      blockIndex = 1;
+      jumpToMenu.options.push("menus." + rulesMenu.id);
+    }
+
+    if (subroutines.length > 0) {
+      let subroutinesMenu = plugin.createMenu(
+        "subroutineMenu",
+        "Subroutines",
+        _Blockly.ContextMenuRegistry.ScopeType.WORKSPACE
+      );
+      plugin.registerMenu(subroutinesMenu);
+      subroutinesMenu.options = [];
+      subroutines.forEach((block) => {
+        let blockItem = {
+          displayText: block.getFieldValue("SUBROUTINE_NAME"),
+          preconditionFn: () => "enabled",
+          callback: () => {
+            jumpTo(block);
+          },
+          scopeType: _Blockly.ContextMenuRegistry.ScopeType.WORKSPACE,
+          id: block.id,
+          weight: blockIndex,
+        };
+        subroutinesMenu.options.push("items." + blockItem.id);
+        plugin.registerItem(blockItem);
+        blockIndex += 1;
+      });
+      blockIndex = 1;
+      jumpToMenu.options.push("menus." + subroutinesMenu.id);
+    }
+
+    jumpToMenu.weight = 99;
+
+    if (_Blockly.ContextMenuRegistry.registry.getItem(jumpToMenu.id)) {
+      _Blockly.ContextMenuRegistry.registry.unregister(jumpToMenu.id);
+    }
+    _Blockly.ContextMenuRegistry.registry.register(jumpToMenu);
+  }
+
+  function jumpTo(block) {
+    var mWs = _Blockly.getMainWorkspace();
+    var xy = block.getRelativeToSurfaceXY();
+    var m = mWs.getMetrics();
+
+    mWs.scrollbar.set(
+      xy.x * mWs.scale + m.viewWidth * 0.4,
+      xy.y * mWs.scale + m.viewHeight * 0.4
+    );
+
+    block.select();
+  }
+
   const toggleScrollItem = {
     displayText: "Toggle Scroll Workspace",
     preconditionFn: () => "enabled",
@@ -42,7 +159,11 @@
     try {
       _Blockly.getMainWorkspace().options.moveOptions.wheel =
         pluginData.scrollableWorkspace;
+      if (_Blockly.ContextMenuRegistry.registry.getItem(toggleScrollItem.id)) {
+        _Blockly.ContextMenuRegistry.registry.unregister(toggleScrollItem.id);
+      }
       _Blockly.ContextMenuRegistry.registry.register(toggleScrollItem);
+      buildJumpToMenu();
       logInfo("Initialized!");
     } catch (exception) {
       logError("Failed to initialize!", exception);
